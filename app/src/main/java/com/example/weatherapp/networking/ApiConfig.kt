128de07bf8e0815dd438d148d028dc1ec00a1c22
Revolt-47 <i201806@nu.edu.pk>
2023-03-10 18:21:39 +0500

    MyWeatherApp

---
+++ app/src/main/java/com/example/weatherapp/networking/ApiConfig.kt
@@ -0,0 +1,32 @@
+package com.example.weatherapp.networking
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+    companion object {
+        fun getApiService(): ApiService {
+
+            // API response interceptor
+            val loggingInterceptor = HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            // Client
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            // Retrofit
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.weatherapi.com/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+
+        const val API_KEY = "1c432f26a7014cf9bab112217231003";
+    }
+}
---
+++ app/src/main/java/com/example/weatherapp/networking/ApiService.kt
@@ -0,0 +1,16 @@
+package com.example.weatherapp.networking
+
+import com.example.weatherapp.model.CurrentWeatherResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+
+    // Get current weather data
+    @GET("current.json")
+    fun getCurrentWeather(
+        @Query("key") key: String = ApiConfig.API_KEY,
+        @Query("q") city: String,
+        @Query("aqi") aqi: String = "no"
+    ): Call<CurrentWeatherResponse>
+}
